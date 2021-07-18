bd18526ccd8fa2e0fb4fb86150cfdae843d2f14d
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2021-07-18 15:48:38 -0300

    Finalizando aula de mapas/dicionarios

--- Sessão 7/mapas.py
+++ Sessão 7/mapas.py
@@ -15,5 +15,50 @@ for chave in meses:
     print(f"Em {chave} recebi R${meses[chave]},00")
 
 # Função para imprimir chaves
-print(meses.keys()) # Isto irá imprimir
+print(meses.keys()) # Isto irá imprimir as chaves do dicionário
+
+"""
+Forma correta em Python para acessar chaves
+"""
+print()
+print("Forma correta de acessar chaves em Python:")
+meses = dict(jan='100', fev='250', mar='400')
+
+for chaves in meses.keys():
+    print(chaves)
+
+"""
+Forma correta em Python de acessar valores
+"""
+print()
+print("Forma correta de acessar valores em Python:")
+
+print(meses.values())
+
+for valores in meses.values():
+    print(valores)
+
+###############################################################
+"""
+Desempacotamento em dicionários
+"""
+print()
+print("Desempacotamento de dicionários")
+luas = dict(saturn='Ganimedes', jupiter='Galileanas', terra='Lua')
+print(luas.items())
+
+for chave, valor in luas.items():
+    print(chave, '->', valor)
+###############################################################
+"""
+Valor máximo, minimo e tamanho
+* O comando 'sum' só pode ser utilizado em valores INT ou FLOAT
+"""
+print()
+print("Valores máximo, mínimo e o tamanho")
+satelites = dict(saturn='Ganimedes', jupiter='Galileanas', terra='Lua')
+
+# print(max(satelites)) - Deve ser utilizado com INT e FLOAT
+# print(min(satelites)) - Deve ser utilizado com INT e FLOAT
+print(len(satelites))
 
