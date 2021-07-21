d6498e736c8711f30e070f5b3dc9d50fae311e6e
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2021-07-21 12:54:20 -0300

    Conjuntos / Projetos Dra. Angela

---
+++ Projetos/projeto1_nomeDeBanda.py
@@ -0,0 +1,7 @@
+print('Projeto nome de banda')
+print()
+
+cidade = input('Informe sua cidade natal: \n')
+pet = input('Informe qual é seu pet: \n')
+
+print(f'O nome da sua banda é {pet} de {cidade}')
--- Sessão 7/conjuntos.py
+++ Sessão 7/conjuntos.py
@@ -11,7 +11,7 @@
     - Sets tem apenas valor
 """
 # Forma 1
-s = set({1, 2, 3, 4, 5, 5, 6, 7, 3, 2, 1})
+s = set({1, 2, 3, 4, 5, 5, 6, 7, 3, 2, 1}) # Menos comum
 
 print(s)
 print(type(s))
@@ -26,9 +26,54 @@ s = {1, 2, 3, 3, 2, 1}
 print(s)
 print(type(s))
 
+"""
+Para verificar se tem algum número no conjunto, utilziar o if
+"""
+print()
+conj = {1, 2, 3}
 
+if 3 in conj:
+    print('Tem o 3')
+else:
+    print('Não tem o 3')
 
+#################################################################
+"""
+Não tem como ordenar utilizando o sort()
 
+ex: 
+conj = {1, 3, 2, 1, 4, 5}
+conj.sort()
 
+print(conj)
+"""
+#################################################################
+"""
+Revisão de informações
+"""
 
+dados = 3, 4, 4, 5, 99, 99, 12, 15, 15, 14
+print()
+print(f'Total de {len(dados)} elementos')
+print()
+
+# Aceitam valores duplicados
+lista = list(dados)
+print(f'Lista: {lista} com {len(lista)} elementos')
+print(type(lista))
+
+# Aceitam valores duplicados
+tupla = tuple(dados)
+print(f'Tupla: {tupla} com {len(tupla)} elementos')
+print(type(tupla))
+
+# Não aceitam chaves duplicadas
+dicio = {}.fromkeys(dados, 'dados')
+print(f'Dicionário: {dicio} com {len(dicio)} elementos')
+print(type(dicio))
+
+# Não aceitam valores duplicados
+conj = set(dados)
+print(f'Conjuntos: {conj} com {len(conj)} elementos')
+print(type(conj))
 
